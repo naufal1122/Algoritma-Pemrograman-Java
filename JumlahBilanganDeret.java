34cbe486cb4701688b7c2282f31492148b595688
dewantoro_naufal007 <naufalbismania@gmail.com>
2021-11-08 10:15:40 +0700

    naufal

---
+++ JumlahBilanganDeret.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JumlahBilanganDeret {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah bilangan ke-n: ");
+        
+        int n = input.nextInt();
+        int hasilJml=0;
+        String deret="";
+
+        for (int i=1; i<=n; i++) {
+            deret=deret+String.valueOf(i)+" ";
+            hasilJml=hasilJml+i;
+        }
+
+        System.out.println("hasil jumlah bil.asli : "+deret+" adalah "+hasilJml);
+    }
+}
+    
